562f88464f3b9bd94741b7fed3dbb1160219e8d7
airevannaser <108552182+airevannaser@users.noreply.github.com>
2022-07-07 21:34:28 -0700

    Update Renastech2.java

--- practice/Renastech2.java
+++ practice/Renastech2.java
@@ -2,6 +2,6 @@ package practice;
 
 public class Renastech2 {
     public static void main(String[] args) {
-        System.out.println("username for Renastech=abcd");
+        System.out.println("username for Renastech=Airevan");
     }
 }
